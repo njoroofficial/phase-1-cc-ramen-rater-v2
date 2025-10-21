845f6d7b1537ad545fb265e6c5620633ee20b334
njoroofficial <mosesn579@gmail.com>
2025-10-21 08:56:30 +0300

    feat:Implemented the displayRamens function to display ramens images

--- src/index.js
+++ src/index.js
@@ -7,23 +7,39 @@ const handleClick = (ramen) => {
 
 const addSubmitListener = () => {
   // Add code
-}
+};
 
 const displayRamens = () => {
-  // Add code
+  try {
+    // Fetch all ramens from the API
+    fetch("http://localhost:3000/ramens")
+      .then((res) => res.json())
+      .then((ramens) => {
+        // get the ramen-menu div
+        const ramenMenu = document.getElementById("ramen-menu");
+
+        // Loop through each ramen and create an img element
+        ramens.forEach((ramen) => {
+          const img = document.createElement("img");
+          img.src = ramen.image;
+          img.alt = ramen.name;
+
+          // Append the image to the ramen-menu div
+          ramenMenu.appendChild(img);
+        });
+      });
+  } catch {
+    (error) => console.error("Error fetching ramens:", error);
+  }
 };
 
 const main = () => {
   // Invoke displayRamens here
+  displayRamens();
   // Invoke addSubmitListener here
-}
+};
 
-main()
+main();
 
 // Export functions for testing
-export {
-  displayRamens,
-  addSubmitListener,
-  handleClick,
-  main,
-};
+export { displayRamens, addSubmitListener, handleClick, main };
